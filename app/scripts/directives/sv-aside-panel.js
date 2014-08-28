dc5c0149e51635e7b448b9ae702f92a47d8ca5c8
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-08-28 15:39:05 -0500

    Main view functionality using directives

---
+++ app/scripts/directives/sv-aside-panel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('fengshui')
+    .directive('svAsidePanel', function () {
+        return {
+            templateUrl: '../views/directives/sv-aside-panel.html',
+            restrict: 'E'
+        };
+    });

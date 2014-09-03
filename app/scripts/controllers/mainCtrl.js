a174e6a7a740f31c4768bf12a302d22ab39c79f9
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-03 16:09:41 -0500

    Fix with MainCtrl

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,23 +1,40 @@
 'use strict';
 
 angular.module('app')
-    .controller('MainCtrl', function ($scope, $firebase, url, $firebaseSimpleLogin) {
+  .controller('MainCtrl', function ($scope, $firebase, url, $firebaseSimpleLogin, $state) {
+    $scope.$watch('auth.user.provider', function (newVal) {
+      if (newVal === 'password') {
+        $scope.isAdmin = true;
+        console.log('Admin');
+      }
+    })
         // now we can use $firebase to synchronize data between clients and the server!
         var mainRef = new Firebase(url);
         $scope.auth = $firebaseSimpleLogin(mainRef);
 
+
         $scope.auth.$getCurrentUser().then(function (user) {
-            if (!_.isNull(user)) {
-                $scope.email = user.email;
+          if (user === null) {
+            return;
+          }
+          if (user.provider === 'facebook') {
                 $scope.userLink = user.thirdPartyUserData.link;
                 $scope.userName = user.thirdPartyUserData.first_name;
                 $scope.userPic = user.thirdPartyUserData.picture.data.url;
             }
+          if (user.provider === 'password') {
+            $scope.isAdmin = true;
+          }
+
         })
 
         $scope.fenLogout = function () {
             $scope.auth.$logout();
-            console.log('No current user');
+          $scope.userLink = null;
+          $scope.userName = null;
+          $scope.userPic = null;
+
+          $state.go('app.home', null, {reload: true});
         }
 
         $scope.loginFb = function () {

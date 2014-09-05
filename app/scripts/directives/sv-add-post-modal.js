5b208a56594090639f8fa5e5f288f1d45e6a5337
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-05 13:53:52 -0500

    Add post panel

--- app/scripts/directives/sv-add-post-modal.js
+++ app/scripts/directives/sv-add-post-modal.js
@@ -5,21 +5,43 @@ angular.module('app')
     return {
       restrict: 'E',
       scope: {},
-      template: '<button ng-click="showModal()" class="btn btn-link btn-link-dark"><i class="fa fa-pencil"></i> Add New Post</button>',
+	    template: '<button ng-click="showModal()" class="btn btn-link"><i class="fa fa-pencil"></i> Add New Post</button>',
       controller: function ($scope) {
 
       },
       link: function postLink($scope, element, attrs) {
+
+	      $scope.post = {};
+	      var repo = url + 'posts';
+	      $scope.posts = $firebase(new Firebase(repo)).$asArray();
+
+
         var postModal = $modal(
           {
+	          scope: $scope,
             title: 'Add new Post',
             animation: 'am-fade-and-slide-top',
             template: '../../views/modals/sv-add-article.html',
             show: false
           });
+
         $scope.showModal = function () {
           postModal.show();
-        }
+        };
+
+	      $scope.addPost = function (title, author, post) {
+		      $scope.posts.$add(
+			      {
+				      title: title,
+				      author: author,
+				      post: post
+			      }).then(function () {
+				      $scope.post = {};
+				      postModal.hide();
+
+			      });
+	      };
+
       }
     };
   });

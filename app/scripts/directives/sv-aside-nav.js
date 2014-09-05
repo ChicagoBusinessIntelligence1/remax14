503fb62939268ba24dbe3c212a56ef84e43eae68
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-05 10:53:44 -0500

    main content styles

--- app/scripts/directives/sv-aside-nav.js
+++ app/scripts/directives/sv-aside-nav.js
@@ -4,6 +4,14 @@ angular.module('app')
     .directive('svAsideNav', function () {
         return {
             templateUrl: '../views/directives/sv-aside-nav.html',
-            restrict: 'E'
+            restrict: 'E',
+            controller: function($scope){
+                $scope.sub1Shown=false;
+
+                $scope.showSub1 = function () {
+
+                $scope.sub1Shown= !$scope.sub1Shown;
+                };
+            }
         };
     });

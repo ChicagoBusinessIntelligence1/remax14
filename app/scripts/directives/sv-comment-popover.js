95b9b101821f96d502a412efcff63c00185b4717
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-06 18:08:18 -0500

    Edit comment popover design finished

--- app/scripts/directives/sv-comment-popover.js
+++ app/scripts/directives/sv-comment-popover.js
@@ -1,20 +1,19 @@
 'use strict';
 
 angular.module('app')
-    .directive('svCommentPopover', function ($popover) {
-        return {
-            restrict: 'E',
-            scope: {
-                comment: '='
-            },
-            template: '<p class="comment editable"> {{comment}}</p>',
-            link: function postLink($scope, element, attrs) {
-                var myPopover = $popover(element, {
-                    scope:$scope,
-                    title: 'My Title',
-                    content: 'My Content',
-                    template: '../../views/directives/sv-comment-popover.html'
-                });
-            }
-        };
-    });
+  .directive('svCommentPopover', function ($popover) {
+    return {
+      restrict: 'E',
+      scope: {
+        comment: '='
+      },
+      template: '<p class="comment editable"> {{comment}}</p>',
+      link: function postLink($scope, element, attrs) {
+        var myPopover = $popover(element, {
+          scope: $scope,
+          placement: 'top',
+          template: '../../views/directives/sv-comment-popover.html'
+        });
+      }
+    };
+  });

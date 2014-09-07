65994d83223ff8a29af824a7c779a811f96e1e1f
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-06 23:11:10 -0500

    Edit Works

--- app/scripts/directives/sv-comment-popover.js
+++ app/scripts/directives/sv-comment-popover.js
@@ -1,19 +1,32 @@
 'use strict';
 
 angular.module('app')
-  .directive('svCommentPopover', function ($popover) {
+  .directive('svCommentPopover', function ($popover, $firebase, url) {
     return {
       restrict: 'E',
       scope: {
-        comment: '='
+        comment: '=',
+        postKey: '=',
+        commentKey: '='
       },
       template: '<p class="comment editable"> {{comment}}</p>',
       link: function postLink($scope, element, attrs) {
-        var myPopover = $popover(element, {
+
+        $scope.popoverEdit = $popover(element, {
           scope: $scope,
           placement: 'top',
           template: '../../views/directives/sv-comment-popover.html'
         });
+
+        $scope.saveComment = function (comment) {
+          var repo = url + 'posts';
+          var commentsUrl = repo + '/' + $scope.postKey + '/comments/' + $scope.commentKey;
+          $scope.comments = $firebase(new Firebase(commentsUrl));
+          $scope.comments.$update({comment: comment}).then(function () {
+            $scope.popoverEdit.hide();
+            toastr.info('post has been updated');
+          })
+        };
       }
     };
   });

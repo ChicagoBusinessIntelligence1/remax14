d2f31078799840cbfa04752998cc8ea3efc778b9
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-14 11:51:40 -0500

    Post Types

--- app/scripts/directives/sv-add-article.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app')
-    .directive('svAddArticle', function () {
-        return {
-            templateUrl: '../views/directives/sv-add-article.html',
-            restrict: 'E'
-        };
-    });
--- app/scripts/directives/sv-add-post-modal.js
+++ app/scripts/directives/sv-add-post-modal.js
@@ -4,21 +4,24 @@ angular.module('app')
   .directive('svAddPostModal', function ($modal, $firebase, url) {
     return {
       restrict: 'E',
-        scope: {
-            posts: '='
-        },
-        template: '<button ng-click="showModal()" class="btn btn-link btn-link-dark"><i class="fa fa-pencil"></i> Add New Post</button>',
+      scope: {
+        posts: '='
+      },
+      template: '<button ng-click="showModal()" class="btn btn-link btn-link-dark"><i class="fa fa-pencil"></i> Добавить Пост</button>',
       controller: function ($scope) {
 
       },
       link: function postLink($scope, element, attrs) {
 
-          $scope.post = {};
+        var repo = url + 'elements';
+        $scope.elements = $firebase(new Firebase(repo)).$asArray();
+
+        $scope.post = {};
 
         var postModal = $modal(
           {
-              scope: $scope,
-            title: 'Add new Post',
+            scope: $scope,
+            title: 'Добавить Пост',
             animation: 'am-fade-and-slide-top',
             template: '../../views/modals/sv-add-article.html',
             show: false
@@ -28,20 +31,20 @@ angular.module('app')
           postModal.show();
         };
 
-          $scope.addPost = function (title, author, body) {
-              $scope.posts.$add(
-                  {
-                      title: title,
-                      author: author,
-                      body: body,
-                      time: (new Date()).getTime()
-                  }).then(function () {
-                      $scope.post = {};
-                      postModal.hide();
-                      toastr.info('The post has been submitted');
+        $scope.addPost = function (title, author, body) {
+          $scope.posts.$add(
+            {
+              title: title,
+              author: author,
+              body: body,
+              time: (new Date()).getTime()
+            }).then(function () {
+              $scope.post = {};
+              postModal.hide();
+              toastr.info('The post has been submitted');
 
-                  });
-          };
+            });
+        };
 
       }
     };

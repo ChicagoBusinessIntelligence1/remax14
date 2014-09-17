6b25baa9a467d2390abc3324b60e0baa6fb09777
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-17 15:04:35 -0500

    Gulp/Angular value

--- gulp/ng/value.js
+++ gulp/ng/value.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 //<editor-fold desc="REQUIRES">
 require('./util.js');
 var gulp = require('gulp');
@@ -11,6 +10,8 @@ var inject = require('gulp-inject-string');
 var gulpif = require('gulp-if');
 var gulpIgnore = require('gulp-ignore');
 var gulpFilter = require('gulp-filter');
+var stripLine = require('gulp-strip-line');
+var replace = require('gulp-replace');
 //</editor-fold>
 
 var scripts = 'app/scripts/';
@@ -21,36 +22,33 @@ var argv = require('yargs')
   .default('value', 'value')
   .argv;
 
-var injectLine = "app.value('" + argv.name + "', '" + argv.value + "');\r\n"
-var injectDep = argv.name;
+var injectLine = "app.value('" + argv.name + "', '" + argv.value + "');\r\n";
 
-var isInjectInFile = argv.file ? true : false;
+var rm = argv.rm;
 
 gulp.task('fileInject', function () {
-  var fileInc = '**/' + argv.file;
+  var fileInc = '**/' + argv.file + '.js';
   var filter = gulpFilter(fileInc);
 
+  var injString = ', ' + argv.name;
   return gulp.src(allJsFiles)
     .pipe(filter)
-    .pipe(print())
+    .pipe(gulpif(rm != true, inject.after('$scope', injString), replace(injString, '')))
+    .pipe(gulp.dest(scripts))
 
   //.pipe(gulp.dest(scripts));
 
 });
 gulp.task('appInject', function () {
   return gulp.src(scripts + 'app.js')
-    .pipe(inject.append(injectLine))
+    .pipe(gulpif(rm != true, inject.append(injectLine), replace(injectLine, '')))
     .pipe(gulp.dest(scripts));
 
-});
-gulp.task('task', function () {
-  var filter = gulpFilter('**/home.js');
-
-  gulp.src('./app/scripts/**/*.js')
-    .pipe(filter)
-    .pipe(print())
 });
 
 
 gulp.task('value', ['appInject', 'fileInject']);
 
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}

23eb4c0578159d0093b5f5ade777d4f1107619e5
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-09-18 14:33:49 -0500

    fensite-26 Bug with displaying new post

---
+++ app/scripts/directives/sv-test3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('app')
+  .directive('svTest3', function () {
+    return {
+      templateUrl: '../views/directives/sv-test3.html',
+      restrict: 'E'
+    };
+  });
---
+++ app/scripts/directives/sv-test4.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('app')
+  .directive('svTest4', function () {
+    return {
+      templateUrl: '../views/directives/sv-test4.html',
+      restrict: 'E'
+    };
+  });

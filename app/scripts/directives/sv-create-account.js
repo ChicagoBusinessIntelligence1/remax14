bcc44e78ffb9073d4fb60e0c3a2f7410656e03aa
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-05 12:23:00 -0600

    remax14-41 Create your account form design

---
+++ app/scripts/directives/sv-create-account.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('app')
+  .directive('svCreateAccount', function () {
+    return {
+      restrict: 'E',
+      templateUrl: '../../views/directives/sv-create-account.html',
+      scope: {
+
+      },
+      link: function ($scope, element, attr) {
+
+      }
+    };
+  });

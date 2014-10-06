d7e623afcdcaf2a7592df62207353d5d54df1037
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-06 12:44:40 -0600

    remax14-44 Directive for a button

--- app/scripts/directives/sv-btn-room.js
+++ app/scripts/directives/sv-btn-room.js
@@ -1,19 +1,13 @@
 'use strict';
 
 angular.module('app')
-  .directive('svCheckbox', function () {
+  .directive('svBtnRoom', function () {
     return {
       restrict: 'E',
       replace: true,
-      template:
-      '<ul class="list-unstyled">' +
-      '<li><h5>{{titleCap}}:</h5></li>' +
-      '<li><input type="checkbox" name="{{nameId}}" id="{{nameId}}"/>' +
-      '</li>' +
-      '</ul>',
-      scope: {
-        title: '@'
-      },
+      template: '<button class="btn btn-primary btn-sm" name="{{nameId}}" id="{{nameId}}">{{titleCap}}</button>',
+      scope: {},
+
       link: function ($scope, element, attr) {
         $scope.titleCap = $scope.title.charAt(0).toUpperCase() + $scope.title.slice(1);
         $scope.nameId = $scope.title.split(' ').join('-').toLowerCase();

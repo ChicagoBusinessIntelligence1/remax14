a6d676260a899c7c326e9df130e37857abdb3c3c
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-06 12:28:06 -0600

    remax14-43 Directive for a checbox

---
+++ app/scripts/directives/sv-checkbox.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('app')
+  .directive('svCheckbox', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      template:
+      '<ul class="list-unstyled">' +
+      '<li><h5>{{titleCap}}:</h5></li>' +
+      '<li><input type="checkbox" name="{{nameId}}" id="{{nameId}}"/>' +
+      '</li>' +
+      '</ul>',
+      scope: {
+        title: '@'
+      },
+      link: function ($scope, element, attr) {
+        $scope.titleCap = $scope.title.charAt(0).toUpperCase() + $scope.title.slice(1);
+      }
+    };
+  });

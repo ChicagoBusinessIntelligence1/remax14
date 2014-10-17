5a759d35ef2f9c5bdc71154c2e16ec4514acd181
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-10-17 14:48:40 -0500

    Saving to Db Works.

--- app/scripts/controllers/ListingDrafts.js
+++ app/scripts/controllers/ListingDrafts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .controller('ListingDraftsCtrl', function ($scope, $firebase, urlResidential,urlBrokers, $rootScope) {
+  .controller('ListingDraftsCtrl', function ($scope, $firebase, urlResidential, urlBrokers, $rootScope) {
     $rootScope.auth.$getCurrentUser().then(function (user) {
       var draftsRepo = urlBrokers + user.id + '/drafts/residential/';
       $scope.draftsRef = $firebase(new Firebase(draftsRepo));
@@ -12,12 +12,10 @@ angular.module('app')
       };
       $scope.postDraft = function (mls) {
         var draft = $scope.drafts.$getRecord(mls);
-        console.log(draft);
-        var listingsRepo = urlResidential;
-        $scope.listingsRef = $firebase(new Firebase(listingsRepo));
-        $scope.listings = $scope.listingsRef.$asObject();
-        $scope.listings[mls] = draft;
-        $scope.listings.$save(mls);
+        draft = _.omit(draft, ['$id','$priority','$$hashKey']);
+        $scope.listingsRef = $firebase(new Firebase(urlResidential + mls));
+        $scope.listingsRef.$set(draft);
+
         $scope.draftsRef.$remove(mls);
       };
     })

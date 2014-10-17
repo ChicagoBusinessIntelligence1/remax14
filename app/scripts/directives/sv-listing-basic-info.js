1be0bc5ad1ccdcc48fdea34e8ea7a7b7477f388c
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-17 14:44:23 -0500

    remax14-63 Displaying small listing info directive

--- app/scripts/directives/sv-listing-basic-info.js
+++ app/scripts/directives/sv-listing-basic-info.js
@@ -6,10 +6,9 @@ angular.module('app')
       restrict: 'E',
       templateUrl: '../../views/directives/sv-listing-basic-info.html',
       scope: {
-
+        listing: '='
       },
       link: function ($scope, element, attr) {
-
       }
     };
   });

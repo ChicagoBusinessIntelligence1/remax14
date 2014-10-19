fedcc8c89284d91a159f961fba21a8be8096da92
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-10-19 13:19:02 -0500

    Add files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,5 @@
+/// <reference path="services/MyService.js" />
+profile.service("MyService", MyService);
 /// <reference path="services/MyseerService.js" />
 profile.service("MyseerService", MyseerService);
 'use strict';

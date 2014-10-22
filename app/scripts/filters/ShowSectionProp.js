d601dd2fc1877fd8c3902468eaed59b7b0c57794
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-10-22 10:03:14 -0500

    Fix Property filter

--- app/scripts/filters/ShowSectionProp.js
+++ app/scripts/filters/ShowSectionProp.js
@@ -3,11 +3,17 @@
 angular.module('app')
   .filter('showSectionProp', function () {
     return function (home, propName) {
+      var foundProp;
       for (var i = 0; i < home.length; i++) {
         var section = home[i];
         var props = section.content;
-        var prop = _.find(props, function (el) { return el.title === propName; });
-      return prop.value;
+        foundProp = _.find(props, function (el) {
+          return el.title === propName;
+        });
+        if (!_.isUndefined(foundProp)) {
+          break;
+        }
       }
+      return foundProp ? foundProp.value : '';
     };
   });

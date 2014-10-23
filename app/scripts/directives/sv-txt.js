b371b1590d58120bb6d8134e23deb1b3777ef93a
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-10-23 11:12:42 -0500

    Fix Property filter

--- app/scripts/directives/sv-txt.js
+++ app/scripts/directives/sv-txt.js
@@ -6,7 +6,8 @@ angular.module('app')
       restrict: 'E',
       replace: true,
       template: '<div class="form-group txt-group">' +
-      '<input ng-class="{error:isMaxInvalid}" ng-model="model.value"  type="text" name="{{nameId}}" id="{{nameId}}" class="form-control" ng-required="r" /> ' +
+      '<input ng-class="{error:isMaxInvalid}" ng-model="model.value" ng-if="isText"   type="text" name="{{nameId}}" id="{{nameId}}" class="form-control" ng-required="r" /> ' +
+      '<input ng-class="{error:isMaxInvalid}" ng-model="model.value"  ng-if="!isText"  type="number" name="{{nameId}}" id="{{nameId}}" class="form-control" ng-required="r" /> ' +
       '<span ng-show="r && !model.value.length" class="notice error-note">Required</span>' +
       '</div>',
       scope: {
@@ -14,6 +15,7 @@ angular.module('app')
         r: '='
       },
       link: function ($scope, element, attr) {
+        $scope.isText = $scope.sectionProperty.title !== 'price';
 
         $scope.model = $scope.sectionProperty;
         $scope.nameId = _.str.camelize($scope.sectionProperty.title.split(' ').join('-').toLowerCase());
--- app/scripts/services/SearchService.js
+++ app/scripts/services/SearchService.js
@@ -14,15 +14,48 @@ angular.module('app')
         that.repoUrl = url.residential;
         that.repoRef = $firebase(new Firebase(that.repoUrl));
         var allHomes = that.repoRef.$asArray();
-        var finalHomes = [];
-        allHomes.$loaded(function () {
-          for (var i = 0; i < allHomes.length; i++) {
-            var home = allHomes[i];
+        if (_.isUndefined(query)) {
+          defered.resolve(that.repoRef.$asArray());
+        } else {
+          var finalHomes = [];
+          allHomes.$loaded(function () {
 
-          }
+            for (var i = 0; i < allHomes.length; i++) {
+              var home = allHomes[i];
+              var isHomeIncluded = true;
+              for (var j = 0; j < home.length; j++) {
+                var section = home[j];
+                var sectionProps = section.content;
+                for (var k = 0; k < sectionProps.length; k++) {
+                  var property = sectionProps[k];
 
-        })
-        defered.resolve(that.repoRef.$asArray());
+                  switch (property.title) {
+                    case 'city':
+                    case 'state':
+                    case 'zip':
+                      if (property.value.indexOf(query.location) === -1) {
+                        isHomeIncluded = false;
+                      }
+                      break;
+
+                    case'price':
+                      var housePrice = parseInt(property.value);
+                      var maxPrice = parseInt(query.priceMax);
+                      var minPrice = parseInt(query.priceMin);
+                      if (housePrice < maxPrice || housePrice >= minPrice) {
+                        isHomeIncluded = false;
+                      }
+                      break;
+
+                  }
+                }
+
+              }
+            }
+
+            defered.resolve(finalHomes);
+          });
+        }
 
         return defered.promise;
       }

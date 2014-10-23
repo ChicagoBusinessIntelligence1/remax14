dfae163e0032c143b656264c173e77247aa19f1e
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-23 10:02:23 -0500

    Add files

--- app/scripts/directives/sv-home-basic-info.js
+++ app/scripts/directives/sv-home-basic-info.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .directive('svHomeBasicInfo', function () {
+  .directive('svHomeBasicInfo', function (TestService, ) {
     return {
       restrict: 'E',
       templateUrl: '../../views/directives/sv-home-basic-info.html',

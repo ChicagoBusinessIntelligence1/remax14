8e218472e539e19e97fb0d12ad2d35568e0f228f
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-27 12:12:35 -0500

    remax14-87 Broker Profile Navigation Functionality

--- app/scripts/directives/sv-btn-save-profile-info.js
+++ app/scripts/directives/sv-btn-save-profile-info.js
@@ -5,15 +5,17 @@ angular.module('app')
     return {
       restrict: 'E',
       replace: true,
-      templateUrl: '../../views/directives/sv-btn-save-profile-info.html',
+      template:
+      //'<button ng-click="save()" class="btn btn-info btn-sm" name="{{nameId}}" id="{{nameId}}">' +
+      '<button ng-click="saveProfileInfo()" class="btn btn-info btn-sm" ng-disabled="form.$invalid" name="saveProfileInfo" id="saveProfileInfo">' +
+      '<i class="fa fa-save fa-fw"></i>' +
+      'Save Changes' +
+      '</button>',
       scope: {
 
       },
       link: function ($scope, element, attr) {
 
-      },
-      controller: function ($scope) {
-      //this.var=something;
-      },
+      }
     };
   });

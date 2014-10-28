a776040dda810d4eebb6b16c72cdf5700d0b4d5b
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-28 14:05:32 -0500

    remax14-93 Directive for removing a saved search

--- app/scripts/directives/sv-btn-remove-search.js
+++ app/scripts/directives/sv-btn-remove-search.js
@@ -1,19 +1,28 @@
 'use strict';
 
 angular.module('app')
-  .directive('svBtnRemoveSearch', function () {
+  .directive('svBtnRemoveSearch', function ($popover) {
     return {
       restrict: 'E',
+      template: '<button class="btn btn-default" data-toggle="tooltip" data-placement="top" title="Remove this Search"><i class="fa fa-remove fa-fw"></i></button>',
       replace: true,
-      templateUrl: '../../views/directives/sv-btn-remove-search.html',
-      scope: {
-
-      },
+      scope: {},
       link: function ($scope, element, attr) {
+        $scope.removeSearch = function () {
+          toastr.success('Yor search was successfully removed');
+        };
+        var removeSearch = $popover(element, {
+          container: 'body',
+          template: '../../views/popover/remove-search-popover.html',
+          placement: 'bottom',
+          trigger: 'focus',
+          animation: 'am-flip-x',
+          scope: $scope
+        });
 
       },
       controller: function ($scope) {
-      //this.var=something;
-      },
+        //this.var=something;
+      }
     };
   });
--- app/scripts/directives/sv-customer-info-form.js
+++ app/scripts/directives/sv-customer-info-form.js
@@ -8,16 +8,14 @@ angular.module('app')
       templateUrl: '../../views/directives/sv-customer-info-form.html',
       scope: {},
       link: function ($scope, element, attr) {
-
-        $scope.saveProfileInfo = function () {
-          UserService.saveProfile($scope.customer);
-        };
         $scope.isDataLoading = true;
         UserService.getProfile($rootScope.user.id).$loaded(function (savedUser) {
           var user;
           if (savedUser.$value === null) {
             user = $rootScope.user;
+            $scope.isLocal = false;
           } else {
+            $scope.isLocal = true;
             user = savedUser;
           }
 
@@ -25,11 +23,14 @@ angular.module('app')
             first_name: user.first_name,
             last_name: user.last_name,
             email: user.email,
-            phone: ''
+            phone: user.phone
           };
-        $scope.isDataLoading = false;
+          $scope.isDataLoading = false;
         });
 
+        $scope.saveProfileInfo = function () {
+          UserService.saveProfile($scope.customer);
+        };
       }
     };
   });

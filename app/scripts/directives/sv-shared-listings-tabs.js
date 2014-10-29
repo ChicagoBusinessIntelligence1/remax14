196c2d69cab90a217414dad4fc01863dbb9e67ca
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-10-29 15:14:36 -0500

    remax14-95 Search All homes for Sale

--- app/scripts/directives/sv-shared-listings-tabs.js
+++ app/scripts/directives/sv-shared-listings-tabs.js
@@ -6,14 +6,11 @@ angular.module('app')
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-shared-listings-tabs.html',
-      scope: {
-
-      },
       link: function ($scope, element, attr) {
 
       },
-      controller: function ($scope) {
-      //this.var=something;
-      },
+      controller: function ($scope, $templateCache) {
+
+      }
     };
   });

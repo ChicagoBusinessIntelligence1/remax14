202929c92a3b6cf842635245869881980ed2a583
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-02 10:14:53 -0600

    Tabs Directive

--- app/scripts/directives/sv-drop-down.js
+++ app/scripts/directives/sv-drop-down.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('app')
-  .directive('svDropDown', function () {
+  .directive('svDropDown', function (url, $firebase) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-drop-down.html',
       scope: {
-
+        sectionProperty: '='
       },
       link: function ($scope, element, attr) {
-
+        var dropDownRepo = url.residentialSettings + $scope.sectionProperty.title + 's';
+        console.log(dropDownRepo);
       }
     };
   });

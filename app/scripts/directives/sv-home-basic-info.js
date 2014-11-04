90be0cb16851550ff4b44347dd1389c391339320
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-04 16:27:45 -0600

    Add files

--- app/scripts/directives/sv-home-basic-info.js
+++ app/scripts/directives/sv-home-basic-info.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .directive('svHomeBasicInfo', function () {
+  .directive('svHomeBasicInfo', function ($rootScope) {
     return {
       restrict: 'E',
       templateUrl: '../../views/directives/sv-home-basic-info.html',
@@ -11,6 +11,10 @@ angular.module('app')
       require: '^sv-homes-list',
       link: function ($scope, element, attr, svRentalsCtrl) {
         $scope.isRent = svRentalsCtrl.isRent;
+        $scope.addToWatchList = function (mls) {
+          console.log(mls);
+          
+        };
       }
     };
   });

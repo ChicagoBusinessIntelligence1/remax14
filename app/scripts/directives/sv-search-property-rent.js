dd017b8a28804e932c7058d449a454aaa724f736
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-04 12:00:10 -0600

    remax14-118 Search property for rent functionality

--- app/scripts/directives/sv-search-property-rent.js
+++ app/scripts/directives/sv-search-property-rent.js
@@ -1,16 +1,13 @@
 'use strict';
 
 angular.module('app')
-  .directive('svSearchPropertyRent', function () {
+  .directive('svSearchPropertyRent', function ($state, $rootScope) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-search-property-rent.html',
-      scope: {
 
-      },
       link: function ($scope, element, attr) {
-
       }
     };
   });

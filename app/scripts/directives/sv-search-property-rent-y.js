ad9ac71e5e77e6be604ff0bc0de3a097e90e6c51
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-04 12:21:04 -0600

    Add files

---
+++ app/scripts/directives/sv-search-property-rent-y.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('app')
+  .directive('svSearchPropertyRentY', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '../../views/directives/sv-search-property-rent-y.html',
+      scope: {
+
+      },
+      link: function ($scope, element, attr) {
+
+      }
+    };
+  });

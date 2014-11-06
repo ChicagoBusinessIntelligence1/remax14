cb3b0d1acb09500e65c4274887c488b0cfa38f75
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-06 15:00:31 -0600

    buttons design for drafts

--- app/scripts/directives/sv-btn-add-wish-home.js
+++ app/scripts/directives/sv-btn-add-wish-home.js
@@ -1,16 +1,32 @@
 'use strict';
 
 angular.module('app')
-  .directive('svBtnAddWishHome', function () {
+  .directive('svBtnAddWishHome', function ($modal) {
     return {
       restrict: 'E',
       replace: true,
-      templateUrl: '../../views/directives/sv-btn-add-wish-home.html',
+      template: '<div class="form-group">' +
+      '<button class="btn btn-primary btn-sm" name="{{nameId}}" id="{{nameId}}" ng-click="showModal()" >' +
+      '<i class="fa fa-plus fa-fw"></i>' +
+      'Add Property to Wish List' +
+      '</button>' +
+      '</div>',
       scope: {
-
+        form: '='
       },
       link: function ($scope, element, attr) {
 
+        var addWishHomeModal = $modal(
+          {
+            scope: $scope,
+            title: 'Create a new Wish List Request',
+            animation: 'am-fade-and-slide-top',
+            template: '../../views/modals/add-wish-home-modal.html',
+            show: false
+          });
+        $scope.showModal = function () {
+          addWishHomeModal.$promise.then(addWishHomeModal.show);
+        };
       }
     };
   });

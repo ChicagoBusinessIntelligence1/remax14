288db659c8371f8f26c1e056fc3c32bbf7ade576
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-09 12:21:08 -0600

    remax14-161 Hide Broker Dashboard on Customer Profile

--- app/scripts/controllers/ProfileCtrl.js
+++ app/scripts/controllers/ProfileCtrl.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('app')
-  .controller('ProfileCtrl', function (ProfileTypeService, WatchListService,$scope, $state, user, $rootScope) {
+  .controller('ProfileCtrl', function (ProfileTypeService, WatchListService, $scope, $state, user, $rootScope) {
     if (!_.isEmpty(user)) {
       ProfileTypeService.determine(user).then(function (user) {
         $rootScope.user = user.thirdPartyUserData;
         WatchListService.getSaleRentLists().then(function (watchList) {
           $rootScope.user.watchList = watchList;
         })
-
       })
     }
 

fde71b5c3ff5c3927ce44b55259e4d4a5e72890b
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-11 12:55:55 -0600

    remax14-139 Displaying information for one listing for rent

--- app/scripts/directives/sv-input-numbers.js
+++ app/scripts/directives/sv-input-numbers.js
@@ -7,6 +7,10 @@ angular.module('app')
       link: function ($scope, element, attrs, modelCtrl) {
 
         modelCtrl.$parsers.push(function (inputValue) {
+          if ($scope.inputType !== 'number') {
+            return inputValue;
+          }
+
           inputValue = inputValue.replace(/[^0-9]/g,'');
           modelCtrl.$setViewValue(inputValue);
           modelCtrl.$render();

b3abc1cc65fcb66ede9f3bf27bb9b427baa7d112
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-13 12:15:13 -0600

    Add files

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .controller('MainCtrl', function ($scope, $firebase, mainUrl, $firebaseSimpleLogin, $state, $rootScope) {
+  .controller('MainCtrl', function (AuthService, $scope, $firebase, mainUrl, $firebaseSimpleLogin, $state, $rootScope) {
     $rootScope.logoutFb = function () {
       var mainRef = new Firebase(mainUrl);
       $rootScope.auth = $firebaseSimpleLogin(mainRef);

72350dfce3ba800022dd7089fda882dac0c439c7
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-14 13:04:33 -0600

    Photo Gallery directive design #119

--- app/scripts/directives/sv-photo-gallery.js
+++ app/scripts/directives/sv-photo-gallery.js
@@ -5,11 +5,21 @@ angular.module('app')
     return {
       restrict: 'E',
       replace: true,
-      scope:{
-        home:'='
+      scope: {
+        home: '='
       },
       templateUrl: '../../views/directives/sv-photo-gallery.html',
       link: function ($scope, element, attr) {
+        $scope.$watch('home[0].content.images', function (newValue, oldValue) {
+          if (_.isUndefined(newValue)) {
+            return;
+          }
+          $scope.selectedImage = _.first(newValue);
+        });
+        $scope.setSelectedImage = function (url) {
+          console.log(url);
+          $scope.selectedImage = url;
+        };
 
       }
     };

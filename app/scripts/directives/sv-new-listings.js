f46b18dc6102aecd81ab03b26164e4b706f704d7
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-17 11:21:17 -0600

    skjfksjf

--- app/scripts/directives/sv-new-listings.js
+++ app/scripts/directives/sv-new-listings.js
@@ -6,10 +6,14 @@ angular.module('app')
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-new-listings.html',
-      scope: {
-
-      },
       link: function ($scope, element, attr) {
+        var Transitionable = $famous['famous/transitions/Transitionable'];
+        var Timer = $famous['famous/utilities/Timer'];
+        var Easing = $famous['famous/transitions/Easing'];
+        var EventHandler = $famous['famous/core/EventHandler'];
+
+        $scope.scrollHandler = new EventHandler();
+        $scope.loading = false;
 
       }
     };

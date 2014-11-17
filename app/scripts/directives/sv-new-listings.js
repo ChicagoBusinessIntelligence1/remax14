879a30ce0fa3db1976245bb5599b96aac4d684ab
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-17 12:17:11 -0600

    Sharing Works Corrrectly

--- app/scripts/directives/sv-new-listings.js
+++ app/scripts/directives/sv-new-listings.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .directive('svNewListings', function () {
+  .directive('svNewListings', function ($famous) {
     return {
       restrict: 'E',
       replace: true,

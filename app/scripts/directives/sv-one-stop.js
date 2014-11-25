6a3f5271a4988af67fa141542a040eacd66e9672
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-25 16:03:35 -0600

    Photo-gallery responsive design - design and functionality

--- app/scripts/directives/sv-one-stop.js
+++ app/scripts/directives/sv-one-stop.js
@@ -14,12 +14,35 @@ angular.module('app')
         var Modifier = famous.core.Modifier;
         var Transform = famous.core.Transform;
         var ImageSurface = famous.surfaces.ImageSurface;
+        var Surface = famous.core.Surface;
         var isOrbit = false;
 
 // create the main context
         var ang_element = angular.element(element);
         var orbitDom = ang_element[0];
         var mainContext = Engine.createContext(orbitDom);
+
+        var featureModifier = new Modifier({
+          origin: [0, 1],
+          align: [0.5, 0],
+          size:[150,50],
+          opacity:0
+
+        });
+
+        var firstSurface = new Surface({
+          content: "<div>Buy or Sell Home</div>",
+          properties: {
+            backgroundColor: 'rgb(240, 238, 233)',
+            textAlign: 'left',
+            padding: '5px',
+            border: '2px solid rgb(210, 208, 203)',
+            marginTop: '50px',
+            marginLeft: '50px'
+          }
+        });
+
+        mainContext.add(featureModifier).add(firstSurface);
 
 // your app here
         var logo = new ImageSurface({
@@ -36,21 +59,31 @@ angular.module('app')
             return Transform.rotateY(.002 * (Date.now() - initialTime));
           }
         });
-        var centerSpinModifier = new Modifier({
+        var centerSpinModifierStatic = new Modifier({
           origin: [0.5, 0.5],
           align: [0.5, 0.5]
         });
-        var orbit = $('#rotation-sign');
-        mainContext.add(centerSpinModifier).add(logo);
 
-        orbit.on('mouseover', function () {
+        mainContext.add(centerSpinModifierStatic).add(logo);
+        var isObrbiting = false;
+        var orbit = $('#rotation-sign');
+
+        orbit.on('click', function () {
+          if (isObrbiting) {
+            isObrbiting = false;
+            mainContext.add(centerSpinModifierStatic).add(logo);
+            featureModifier.setOpacity(0, {duration:1000});
+          } else {
+            isObrbiting = true;
+            mainContext.add(centerSpinModifierOrbit).add(logo);
+            featureModifier.setOpacity(1, {duration:1000});
+
+          }
           //centerSpinModifier.setTransform(Transform.rotateY(.002 * (Date.now() - initialTime)));
-        mainContext.add(centerSpinModifierOrbit).add(logo);
         })
-        orbit.on('mouseout', function () {
-          //centerSpinModifier.setTransform(Transform.rotateY(.002 * (Date.now() - initialTime)));
-        mainContext.add(centerSpinModifier).add(logo);
-        })
+        //orbit.on('mouseout', function () {
+        //  //centerSpinModifier.setTransform(Transform.rotateY(.002 * (Date.now() - initialTime)));
+        //})
       }
     };
   });

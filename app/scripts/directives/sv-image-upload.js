61fd3e064321a5e8ec5e767cf98e123691273cde
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-28 12:16:11 -0600

    Add files

--- app/scripts/directives/sv-image-upload.js
+++ app/scripts/directives/sv-image-upload.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .directive('svImageUpload', function (ImageUploadService, FileUploader, imgWidth) {
+  .directive('svImageUpload', function (FileUploader, imgWidth) {
     return {
       restrict: 'E',
       replace: true,

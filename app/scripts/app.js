c02702c41ba3a375fc7cb3ad75b7466f92b6f833
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-11-29 17:08:12 -0600

    My Gallery Images on Modal Window design

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,41 +28,46 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: 'ProfileCtrl',
         templateUrl: '../views/main.html'
       })
+
       .state('app.home', {
         url: '/home',
         templateUrl: '../views/home.html'
+      })
+      .state("app.structure", {
+        abstract: true,
+        templateUrl: "../views/structure.html"
       })
       .state('app.auth', {
         url: '/admin',
         controller: 'LoginCtrl',
         templateUrl: '../views/login.html'
       })
-      .state("app.remax-auth", {
+      .state("app.structure.remax-auth", {
         url: "/remax-auth",
         controller: "RemaxAuthCtrl",
         templateUrl: "../views/remax-auth.html"
       })
-      .state('app.contact', {
+      .state('app.structure.contact', {
         url: '/contact',
         controller: 'ContactUsCtrl',
         templateUrl: '../views/contactus.html'
       })
-      .state('app.remax-articles', {
+      .state('app.structure.remax-articles', {
         url: '/remax-1st-class-articles',
         controller: 'RemaxArticlesCtrl',
         templateUrl: '../views/remax-articles.html'
       })
-      .state('app.agents', {
+      .state('app.structure.agents', {
         url: '/remax-1st-class-agents',
         controller: 'AgentsCtrl',
         templateUrl: '../views/agents.html'
       })
-      .state('app.become-remax-agent', {
+      .state('app.structure.become-remax-agent', {
         url: '/become-remax-1st-class-agent',
         controller: 'BecomeRemaxAgentCtrl',
         templateUrl: '../views/become-remax-agent.html'
       })
-      .state('app.why-remax', {
+      .state('app.structure.why-remax', {
         url: '/why-remax-1st-class',
         controller: 'WhyRemaxCtrl',
         templateUrl: '../views/why-remax.html'
@@ -72,7 +77,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: "GenerateCtrl",
         templateUrl: "../views/generate.html"
       })
-      .state("app.all-sale-homes", {
+      .state("app.structure.all-sale-homes", {
         url: "/all-sale-homes",
         resolve: {
           homes: function (TypeHomesService) {
@@ -82,7 +87,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: "AllSaleHomesCtrl",
         templateUrl: "../views/all-sale-homes-ctrl.html"
       })
-      .state("app.all-rental-homes", {
+      .state("app.structure.all-rental-homes", {
         url: "/all-rental-homes",
         resolve: {
           homes: function (TypeHomesService) {
@@ -93,7 +98,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         templateUrl: "../views/all-rental-homes-ctrl.html"
       })
 
-      .state("app.condos-sale-homes", {
+      .state("app.structure.condos-sale-homes", {
         url: "/condos-sale-homes",
         resolve: {
           homes: function (TypeHomesService) {
@@ -103,7 +108,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: "CondosSaleHomesCtrl",
         templateUrl: "../views/condos-sale-homes-ctrl.html"
       })
-      .state("app.all-rental-houses", {
+      .state("app.structure.all-rental-houses", {
         url: "/all-rental-houses",
         resolve: {
           homes: function (TypeHomesService) {
@@ -113,7 +118,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: "AllRentalHousesCtrl",
         templateUrl: "../views/all-rental-houses-ctrl.html"
       })
-      .state("app.all-rental-apartments", {
+      .state("app.structure.all-rental-apartments", {
         url: "/all-rental-apartments",
         resolve: {
           homes: function (TypeHomesService) {
@@ -124,7 +129,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         templateUrl: "../views/all-rental-apartments-ctrl.html"
       })
 
-      .state("app.single-family-homes", {
+      .state("app.structure.single-family-homes", {
         url: "/single-family-homes",
         resolve: {
           homes: function (TypeHomesService) {
@@ -134,76 +139,76 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: "SingleFamilyHomesCtrl",
         templateUrl: "../views/single-family-homes-ctrl.html"
       })
-      .state("app.profile", {
+      .state("app.structure.profile", {
         abstract: true,
         templateUrl: "../views/profile.html"
       })
-      .state("app.profile.add-home-sale", {
+      .state("app.structure.profile.add-home-sale", {
         url: "/add-home-sale",
         controller: "AddHomeSaleCtrl",
         templateUrl: "../views/add-home-sale-ctrl.html"
       })
-      .state("app.profile.add-home-rental", {
+      .state("app.structure.profile.add-home-rental", {
         url: "/add-home-rental",
         controller: "AddHomeRentalCtrl",
         templateUrl: "../views/add-home-rental-ctrl.html"
       })
-      .state("app.profile.drafts", {
+      .state("app.structure.profile.drafts", {
         url: "/home-drafts",
         controller: "HomeDraftsCtrl",
         templateUrl: "../views/home-drafts-ctrl.html"
       })
-      .state("app.profile.home-sale-draft", {
+      .state("app.structure.profile.home-sale-draft", {
         url: "/home-sale-draft/:mls",
         controller: "HomeSaleDraftCtrl",
         templateUrl: "../views/home-sale-draft-ctrl.html"
       })
-      .state("app.remax-home", {
+      .state("app.structure.remax-home", {
         url: "/remax-home-sale/:mls",
         controller: "HomeSalePostedCtrl",
         templateUrl: "../views/home-sale-posted-ctrl.html"
       })
-      .state("app.profile.home-rent-draft", {
+      .state("app.structure.profile.home-rent-draft", {
         url: "/home-rent-draft/:mls",
         controller: "HomeRentDraftCtrl",
         templateUrl: "../views/home-rent-draft-ctrl.html"
       })
-      .state("app.remax-home-rent", {
+      .state("app.structure.remax-home-rent", {
         url: "/remax-home-rent/:mls",
         controller: "HomeRentPostedCtrl",
         templateUrl: "../views/home-rent-posted-ctrl.html"
       })
-      .state("app.profile.broker-sold-homes", {
+      .state("app.structure.profile.broker-sold-homes", {
         url: "/broker-sold-homes",
         controller: "BrokerSoldHomesCtrl",
         templateUrl: "../views/broker-sold-homes-ctrl.html"
       })
-      .state("app.profile.user-profile", {
+      .state("app.structure.profile.user-profile", {
         url: "/user-profile",
         controller: "UserProfileCtrl",
         templateUrl: "../views/user-profile.html"
       })
-      .state("app.profile.saved-searches", {
+      .state("app.structure.profile.saved-searches", {
         url: "/saved-searches",
         controller: "SavedSearchesCtrl",
         templateUrl: "../views/saved-searches-ctrl.html"
       })
-      .state("app.profile.wish-list", {
+      .state("app.structure.profile.wish-list", {
         url: "/wish-list",
         controller: "WishListCtrl",
         templateUrl: "../views/wish-list-ctrl.html"
       })
-      .state("app.profile.watch-property", {
+      .state("app.structure.profile.watch-property", {
         url: "/watch-property",
         controller: "WatchPropertyCtrl",
         templateUrl: "../views/watch-property-ctrl.html"
       })
-      .state("app.profile.broker-homes", {
+      .state("app.structure.profile.broker-homes", {
         url: "/homes-shared",
         controller: "HomesSharedCtrl",
         templateUrl: "../views/homes-shared-ctrl.html"
       })
-      .state("app.search-sale-results", {
+      .state("app.structure.search-sale-results", {
         url: "/search-sale-results",
         resolve: {
           homes: function (SearchService, urlSale, $rootScope) {
@@ -218,7 +223,7 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         templateUrl: "../views/search-sale-results-ctrl.html"
       })
 
-      .state("app.search-rent-results", {
+      .state("app.structure.search-rent-results", {
         url: "/search-rent-results",
         resolve: {
           homes: function (SearchService, urlRental) {
@@ -233,33 +238,33 @@ var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', '
         controller: "LoginCtrl",
         templateUrl: "../views/login-ctrl.html"
       })
-      .state("app.setting-states-fill", {
+      .state("app.structure.setting-states-fill", {
         url: "/setting-states-fill",
         controller: "SettingStatesFillCtrl",
         templateUrl: "../views/setting-states-fill-ctrl.html"
       })
-      .state("app.homepage-examples", {
+      .state("app.structure.homepage-examples", {
         url: "/homepage-examples",
         controller: "HomepageExamplesCtrl",
         templateUrl: "../views/homepage-examples-ctrl.html"
       })
-      .state("app.svetic", {
+      .state("app.structure.svetic", {
         url: "/svetic",
         controller: "SveticCtrl",
         templateUrl: "../views/svetic-ctrl.html"
       })
-      .state("app.admin", {
+      .state("app.structure.admin", {
         abstract: true,
         url: "/admin",
         controller: "AdminPanelCtrl",
         templateUrl: "../views/admin-panel-ctrl.html"
       })
-      .state("app.admin.broker-applications", {
+      .state("app.structure.admin.broker-applications", {
         url: "/broker-applications",
         controller: "BrokerApplicationsCtrl",
         templateUrl: "../views/broker-applications-ctrl.html"
       })
-      .state("app.admin.registered-brokers", {
+      .state("app.structure.admin.registered-brokers", {
         url: "/registered-brokers",
         controller: "RegisteredBrokersCtrl",
         templateUrl: "../views/registered-brokers-ctrl.html"

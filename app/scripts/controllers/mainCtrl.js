9f8c4e749ea8139db01476aca7f9b55556e74514
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-29 16:26:10 -0600

    Login with firebase 0.9

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,22 +1,30 @@
 'use strict';
 
 angular.module('app')
-  .controller('MainCtrl', function (AuthService, $scope, $firebaseAuth, mainUrl, $state, $rootScope, $location) {
+  .controller('MainCtrl', function (AuthService, ProfileTypeService,WatchListService, $scope, $firebaseAuth, mainUrl, $state, $rootScope, $location) {
 
     $scope.isFirstPage = $location.path() === '/home';
     $rootScope.logoutFb = function () {
-      AuthService.logOut();
+      var ref = new Firebase(mainUrl);
+      $rootScope.authObj = $firebaseAuth(ref);
+      $rootScope.authObj.$unauth();
       $rootScope.user = null;
-      $state.go('home', null, {reload: true});
+
     }
 
     $rootScope.loginFb = function () {
 
       var mainRef = new Firebase(mainUrl);
-      $scope.authObj = $firebaseAuth(mainRef);
+      $rootScope.authObj = $firebaseAuth(mainRef);
 
-      $scope.authObj.$authWithOAuthPopup("facebook").then(function (authData) {
-        $rootScope.user = authData.facebook.cachedUserProfile;
+      $rootScope.authObj.$authWithOAuthPopup("facebook").then(function (authData) {
+        var user = authData.facebook.cachedUserProfile;
+        ProfileTypeService.determine(user).then(function (user) {
+          $rootScope.user = user;
+          WatchListService.getSaleRentLists().then(function (watchList) {
+            $rootScope.user.watchList = watchList;
+          })
+        });
         console.log("Logged in as:", authData.uid);
       }).catch(function (error) {
         console.error("Authentication failed:", error);

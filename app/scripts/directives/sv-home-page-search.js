e0cb0fbdf6000c53619509bcb9ab1577b7280d4d
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-11-30 12:12:46 -0600

    Search works from the home menu

--- app/scripts/directives/sv-home-page-search.js
+++ app/scripts/directives/sv-home-page-search.js
@@ -1,15 +1,33 @@
 'use strict';
 
 angular.module('app')
-  .directive('svHomePageSearch', function ($rootScope, estateTypes) {
+  .directive('svHomePageSearch', function ($rootScope, estateTypes,$state) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-home-page-search.html',
       scope: {},
       link: function ($scope, element, attr) {
-        $scope.
+        var nameType;
+        $scope.property = {value: 'sale'};
+
         $scope.estateTypes = estateTypes;
+
+        $scope.mainSearch = function () {
+
+          if (_.isUndefined($scope.query)) {
+            return;
+          }
+
+          if ($scope.property.value === 'rent') {
+            nameType = 'rent';
+          } else {
+            nameType = 'sale';
+          }
+          var stateName = "app.structure.search-" + nameType + "-results";
+          $rootScope.query = $scope.query;
+          $state.go(stateName, null, {reload: true});
+        };
       }
     };
   });

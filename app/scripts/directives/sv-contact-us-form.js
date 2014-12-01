9504bc2be5dc16d074a03ade4bf2b5f7b43d3f87
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-12-01 16:49:55 -0600

    Email send

--- app/scripts/directives/sv-contact-us-form.js
+++ app/scripts/directives/sv-contact-us-form.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .directive('svContactUsForm', function (notifications, $rootScope) {
+  .directive('svContactUsForm', function (notifications, $rootScope,$http) {
     return {
       restrict: 'E',
       replace: true,
@@ -10,7 +10,22 @@ angular.module('app')
         user: '='
       },
       link: function ($scope, element, attr) {
+
+
+
+        $scope.email={
+          user:$rootScope.user,
+          to:'',
+          subject:'',
+          body:''
+        }
         $scope.sendMessage = function () {
+         var url='https://api.sendgrid.com/api/mail.send.json' ;
+          var data = 'api_user=remax14&api_key=R1eKefo9ApTh&to='+$scope.email.to+'&toname=test &subject='+email.subject+' &text='+$scope.email.body+' &from=remax14@gmail.com'
+          $http.post(url, data);
+
+
+
           toastr.success(notifications.contactMessageSent);
         };
       }

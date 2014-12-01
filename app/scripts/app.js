b17ece5cccc91c890feb8e4b0a38af159655a392
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-12-01 12:33:56 -0600

    Add files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,8 @@
+			.state("app.city-homes-rental", {
+				url: "/city-homes-rental", 
+				controller:"CityHomesRentalCtrl",
+				templateUrl: "../views/city-homes-rental-ctrl.html"
+			})
 'use strict';
 
 var app = angular.module('app', ['angularFileUpload', 'ts.sheets', 'firebase', 'ngMessages', 'ngAnimate', 'famous.angular', 'ngSanitize', 'mgcrea.ngStrap.dropdown', 'mgcrea.ngStrap.modal', 'ui.bootstrap', 'ui.router', 'mgcrea.ngStrap.tooltip', 'mgcrea.ngStrap.helpers.dimensions', 'mgcrea.ngStrap.typeahead', 'mgcrea.ngStrap.popover', 'mgcrea.ngStrap.select', 'mgcrea.ngStrap.datepicker', 'xeditable', 'mgcrea.ngStrap.tab'])
--- app/scripts/controllers/CityHomesCtrl.js
+++ app/scripts/controllers/CityHomesCtrl.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('app')
-  .controller('CityHomesCtrl', function ($scope, $stateParams, SearchService) {
+  .controller('CityHomesCtrl', function ($scope, $stateParams, SearchService, urlSale) {
     var city = $stateParams.city;
     console.time('test1');
-    SearchService.findByCity(city).then(function (homes) {
+    SearchService.findByCity(city, urlSale).then(function (homes) {
       $scope.homes = homes;
       console.timeEnd('test1');
     })

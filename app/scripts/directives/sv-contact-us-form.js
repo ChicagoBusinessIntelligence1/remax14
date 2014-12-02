2351f58140764582ee976a49520451a10671be7a
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-12-02 07:46:23 -0600

    Listing directive functionality

--- app/scripts/directives/sv-contact-us-form.js
+++ app/scripts/directives/sv-contact-us-form.js
@@ -13,13 +13,20 @@ angular.module('app')
 
         $scope.email = {
           user: $rootScope.user,
+          api_user: 'remax14',
+          api_key: 'R1eKefo9ApTh',
+          fromname: $rootScope.user.name,
+          first_name: $rootScope.user.first_name,
+          last_name: $rootScope.user.last_name,
+          to_name: 'Svitlana',
+          to: "chicagobusinessintelligence1@gmail.com",
           from: '',
           subject: '',
           body: ''
         }
         $scope.sendMessage = function () {
           var url = 'https://api.sendgrid.com/api/mail.send.json';
-          var data = 'api_user=remax14&api_key=R1eKefo9ApTh&to=' + '&to=chicagobusinessintelligence1@gmail.com' + '&toname=test &subject=' + $scope.email.subject + ' &text=' + $scope.email.body + '&from=' + $scope.email.from
+          var data = 'api_user=' + $scope.email.api_user + '&api_key=' + $scope.email.api_key+ '&to=' + $scope.email.to + '&toname='  + $scope.email.to_name + '&subject=' + $scope.email.subject + ' &text=' + $scope.email.body + '&from=' + $scope.email.from + '&fromname=' + $scope.email.fromname
           $http.post(url, data);
 
           toastr.success(notifications.contactMessageSent);

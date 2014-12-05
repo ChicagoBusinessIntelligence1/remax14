287ccd433f0a93fd1666e0002995a304b000fbe4
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-12-05 15:17:13 -0600

    Select property functionality for rent #208

--- app/scripts/directives/sv-home-panel.js
+++ app/scripts/directives/sv-home-panel.js
@@ -1,18 +1,27 @@
 'use strict';
 
 angular.module('app')
-  .directive('svHomePanel', function (HousesFrontImagesService,$famous) {
+  .directive('svHomePanel', function (HousesFrontImagesService,$famous,$window) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-home-panel.html',
       link: function ($scope, element, attr) {
+        $scope.viewSize = {
+          width:$window.innerWidth,
+          height:$window.innerHeight
+        }
+        $scope.width=$window.innerWidth;
         HousesFrontImagesService.all().then(function (homes) {
           $scope.homes = homes;
-          console.log($scope.homes);
         })
         var EventHandler = $famous['famous/core/EventHandler'];
 
+        $scope.colors =  ['red', 'green', 'blue', 'orange', 'yellow'];
+        $scope.getColor = function () {
+          var index = Math.floor(Math.random()* 5);
+          return $scope.colors[index];
+        };
         $scope.handler = new EventHandler();
       }
     };

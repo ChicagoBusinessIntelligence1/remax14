393d4a5ef416e418e8ef660a35f62736e019bd5c
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-12-05 10:56:37 -0600

    Add files

--- app/scripts/directives/sv-home-panel.js
+++ app/scripts/directives/sv-home-panel.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('app')
-  .directive('svHomePanel', function () {
+  .directive('svHomePanel', function (HousesFrontImagesService, ) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-home-panel.html',
       link: function ($scope, element, attr) {
+
       }
     };
   });

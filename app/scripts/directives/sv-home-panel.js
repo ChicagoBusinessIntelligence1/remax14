64ad2002463c620d67092f57ccc5f61781d64abc
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-12-07 10:49:56 -0600

    LiveReload works correctly

--- app/scripts/directives/sv-home-panel.js
+++ app/scripts/directives/sv-home-panel.js
@@ -1,28 +1,48 @@
 'use strict';
 
 angular.module('app')
-  .directive('svHomePanel', function (HousesFrontImagesService,$famous,$window) {
+  .directive('svHomePanel', function (HousesFrontImagesService, $famous, $window) {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '../../views/directives/sv-home-panel.html',
       link: function ($scope, element, attr) {
-        $scope.viewSize = {
-          width:$window.innerWidth,
-          height:$window.innerHeight
-        }
-        $scope.width=$window.innerWidth;
+        $scope.showInfo = function (home, $done) {
+          home.opacity.set(1,{duration:1000});
+        };
+
+
+        var Transitionable = $famous['famous/transitions/Transitionable'];
+        var defaultAngle = -Math.PI / 8;
+
+        $scope.w = $window.innerWidth;
+        $scope.h = $window.innerHeight/2;
+
+        $scope.myStyle = {
+          "width" : $scope.w+"px",
+          "height" : $scope.h+"px"
+        };
+
         HousesFrontImagesService.all().then(function (homes) {
-          $scope.homes = homes;
-        })
+          $scope.homes = _.map(homes, function (home) {
+            var angle = new Transitionable(defaultAngle);
+            var opacity = new Transitionable(0);
+
+            return _.extend(home, {
+              angle: angle,
+              opacity:opacity
+            })
+
+          });
+        });
         var EventHandler = $famous['famous/core/EventHandler'];
 
-        $scope.colors =  ['red', 'green', 'blue', 'orange', 'yellow'];
+        $scope.colors = ['red', 'green', 'blue', 'orange', 'yellow'];
         $scope.getColor = function () {
-          var index = Math.floor(Math.random()* 5);
+          var index = Math.floor(Math.random() * 5);
           return $scope.colors[index];
         };
         $scope.handler = new EventHandler();
       }
-    };
+    }
   });

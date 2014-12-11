47f767abae00417df2d8acb22f5077030c4038b9
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-12-11 12:06:44 -0600

    Remax First Class

--- app/scripts/directives/sv-fit-image.js
+++ app/scripts/directives/sv-fit-image.js
@@ -5,11 +5,31 @@ angular.module('app')
     return {
       restrict: 'E',
       replace: true,
-      templateUrl: '../../views/directives/sv-fit-image.html',
+      template: '<div></div>',
+
       scope: {
+        url:'=',
+        w:'@',
+        h:'@'
 
       },
       link: function ($scope, element, attr) {
+        if ($scope.w/2> $scope.h) {
+          $scope.w= $scope.h*2;
+        } else{
+          $scope.h= $scope.w/2;
+        }
+
+        element.css({
+          'background-image': 'url(' + $scope.url + ')',
+          'background-size': '125% auto',
+          'background-position': '50% 25%',
+          'position': 'relative',
+          'height': $scope.h + 'px',
+          'width': $scope.w + 'px'
+        });
+
+
 
       }
     };

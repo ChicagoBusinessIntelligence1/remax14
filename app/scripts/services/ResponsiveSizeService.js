9bf8382110db02a240b2ff1978a9b22d4a17fdac
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-12-14 13:50:20 -0600

    Home page gallery Styles

--- app/scripts/services/ResponsiveSizeService.js
+++ app/scripts/services/ResponsiveSizeService.js
@@ -10,30 +10,34 @@ angular.module('app')
         var config = responsiveGallerySettings;
 
         var percentWidth;
-        // define whether it is large or small device
+        // define width coefficients for large and small devices
         if (window.innerWidth > config.screenSizeLimit) {
-          // full window size coefficient for large devise
           percentWidth = config.largeScreenPart;
         } else {
-          // half window size coefficient for small devise
           percentWidth = config.smallScreenPart;
         }
-        // calculate preliminary parameters width and height to display
-        // gallery background container correctly on any window h and w size
+        //preliminary params for correct proportions on any window size
         var width = percentWidth * window.innerWidth;
         var height = config.percentHeight * window.innerHeight;
-
         if (width > height * config.proportions) {
+          /*if width is our limiting factor, we multiply width by 1.5 to keep proportions 6x4*/
           width = config.proportions * height;
+
+          //responsive design for large devices
           if (width > config.largeScreenPart * data.viewContentWidth) {
+            /*if width of directive is more then half of view content width,
+             * we set width to 50% of container width
+             * then calculate height accordingly to our proportions*/
             width = config.largeScreenPart * data.viewContentWidth;
             height = width / config.proportions;
           }
 
         } else {
+          //if device screen is less then 700, apply normalizing to display gallery correctly
           if (width < config.smallDeviceLimit) {
             width *= config.smallDeviceWidthNormalizer;
           }
+          /*if height is our limiting factor, we multiply height by 1.5 to keep proportions 6x4*/
           height = width / config.proportions;
         }
         //we set w and h on our scope to operate with

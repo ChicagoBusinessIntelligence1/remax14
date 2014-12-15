d19ed46723d8ca39932422a7ba6d2798fd1e6456
AndriyShepitsen <andriy.shepitsen@chicagobusinessintelligence.net>
2014-12-15 11:14:22 -0600

    Add files

---
+++ app/scripts/directives/sv-flip-container.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('app')
+  .directive('svFlipContainer', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '../../views/directives/sv-flip-container.html',
+      scope: {
+
+      },
+      link: function ($scope, element, attr) {
+
+      }
+    };
+  });
